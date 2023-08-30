a6ac33831cd9dd4175444f4b18c98bde835f635c
RebecaFurtado <rebeca.furtado@w2z.com.br>
2023-08-30 11:31:03 -0300

    commit introducao

---
+++ src/components/0-Welcome.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default {
+  title: "Introduction/Bem Vindo",
+};
+
+export const toConnectProject = () => (
+  <div>
+    <h1>Connect Project</h1>
+    <p>
+      Cillum esse excepteur ullamco aute non voluptate ex consectetur non esse.
+      Esse et id aute ipsum exercitation proident commodo labore anim. Enim duis
+      et commodo cillum dolor labore quis reprehenderit qui.
+    </p>
+  </div>
+);
