ee2401d5b932d1f7629c88ebfe4f374ee125ff33
Rebeca1204 <rebeca.furtado@w2z.com.br>
2023-08-31 15:04:25 -0300

    commit add footer component

---
+++ src/components/organisms/Footer.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "./Footer";
+import styled from "styled-components";
+  
+export default {
+  title: "Components/Organisms/Footer",
+  component: Footer,
+};
+  
+export const usage = () => <Footer>
+  
+</Footer>;
