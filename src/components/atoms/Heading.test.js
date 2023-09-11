bfdf299a7488291c4c7581f22a8297c9a7b0c562
Rebeca Mascara <rebecamfurtado@hotmail.com>
2023-09-11 07:38:18 -0300

    commit atualizacao

--- src/components/atoms/Heading.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Heading from "./Heading";
-import { render } from "../../test-utils";
-
-test("match snapshot", () => {
-  const { asFragment } = render(<Heading>Titulo</Heading>);
-  expect(asFragment()).toMatchSnapshot();
-});
--- src/styles/Breakpoints.test.js
+++
@@ -1,9 +0,0 @@
-import { BreakAt, BreakpointSize } from "./Breakpoints";
-test.each([
-  [BreakpointSize.sm],
-  [BreakpointSize.md],
-  [BreakpointSize.lg],
-  [BreakpointSize.xl],
-])("break at %ipx", (size) => {
-  expect(BreakAt(size)).toEqual(`@media (min-width: ${size}px)`);
-});
