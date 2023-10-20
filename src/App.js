b169bd9c4869f2df41660b52fafa6d9d19789168
RebecaFurtado <rebeca.furtado@w2z.com.br>
2023-10-20 11:08:01 -0300

    commit separando routes

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import "./App.css";
 import GlobalStyleComposed from "./styles/GlobalStyle";
 import ThemeProvider from "./styles/ThemeProvider";
-import Home from "./components/pages/Home";
-import Contact from "./components/pages/Contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Produtos from "components/pages/Produtos";
-import MeLiga from "components/pages/MeLiga";
-import Orcamento from "components/pages/Orcamento";
-import Navbar from "components/atoms/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "routes";
 
 function App() {
   return (
     <ThemeProvider>
       <GlobalStyleComposed />
       <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <Home />
-              </>
-            }
-          />
-          <Route
-            path="/produtos"
-            element={
-              <>
-                <Navbar />
-                <Produtos />
-              </>
-            }
-          />
-          <Route
-            path="/contato"
-            element={
-              <>
-                <Navbar />
-                <Contact />
-              </>
-            }
-          />
-          <Route
-            path="/meLigue"
-            element={
-              <>
-                <Navbar />
-                <MeLiga />
-              </>
-            }
-          />
-          <Route
-            path="/orcamento"
-            element={
-              <>
-                <Navbar />
-                <Orcamento />
-              </>
-            }
-          />
-        </Routes>
+        <Routes />
       </Router>
     </ThemeProvider>
   );
---
+++ src/routes/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Produtos from "components/pages/Produtos";
+import MeLiga from "components/pages/MeLiga";
+import Orcamento from "components/pages/Orcamento";
+import Navbar from "components/atoms/Navbar";
+import Home from "components/pages/Home";
+import Contact from "components/pages/Contact";
+
+const AppRoutes = () => (
+  <Routes>
+    <Route
+      path="/"
+      element={
+        <>
+          <Navbar />
+          <Home />
+        </>
+      }
+    />
+    <Route
+      path="/produtos"
+      element={
+        <>
+          <Navbar />
+          <Produtos />
+        </>
+      }
+    />
+    <Route
+      path="/contato"
+      element={
+        <>
+          <Navbar />
+          <Contact />
+        </>
+      }
+    />
+    <Route
+      path="/meLigue"
+      element={
+        <>
+          <Navbar />
+          <MeLiga />
+        </>
+      }
+    />
+    <Route
+      path="/orcamento"
+      element={
+        <>
+          <Navbar />
+          <Orcamento />
+        </>
+      }
+    />
+  </Routes>
+);
+
+export default AppRoutes;
